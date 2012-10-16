11f5d13b5da2ca2ba33f316fefe95fd896ac30ab
antont <toni@playsign>
2012-10-16 10:22:01 +0300

    openni build and conf xml

--- src/Application/OpenNIPlugin/OpenNIPlugin.cpp
+++ src/Application/OpenNIPlugin/OpenNIPlugin.cpp
@@ -4,7 +4,11 @@
 #include "Framework.h"
 #include "CoreDefines.h"
 
-#define SAMPLE_XML_PATH "Sample-User.xml"
+#include <XnOpenNI.h>
+#include <XnCodecIDs.h>
+#include <XnCppWrapper.h>
+
+#define SAMPLE_XML_PATH "data/openni/Sample-User.xml"
 
 #define CHECK_RC(rc, what)											\
 	if (rc != XN_STATUS_OK)											\
@@ -25,17 +29,7 @@
 
 OpenNIPlugin::OpenNIPlugin() : 
   IModule("OpenNIPlugin")
-{
-        /*
-	XnCallbackHandle hUserCBs, hCalibrationStartCB, hCalibrationCompleteCB, hPoseCBs;
-	g_UserGenerator.RegisterUserCallbacks(NewUser, LostUser, NULL, hUserCBs);
-	rc = g_UserGenerator.GetSkeletonCap().RegisterToCalibrationStart(CalibrationStarted, NULL, hCalibrationStartCB);
-	CHECK_RC(rc, "Register to calbiration start");
-	rc = g_UserGenerator.GetSkeletonCap().RegisterToCalibrationComplete(CalibrationCompleted, NULL, hCalibrationCompleteCB);
-	CHECK_RC(rc, "Register to calibration complete");
-	rc = g_UserGenerator.GetPoseDetectionCap().RegisterToPoseDetected(PoseDetected, NULL, hPoseCBs);
-	CHECK_RC(rc, "Register to pose detected");
-        */
+{       
 }
 
 OpenNIPlugin::~OpenNIPlugin()
@@ -45,38 +39,66 @@ OpenNIPlugin::~OpenNIPlugin()
     SAFE_DELETE(skeletonPreviewLabel_);*/
 }
 
+int init() {
+  xn::Context g_Context;
+  xn::ScriptNode g_ScriptNode;
+  xn::DepthGenerator g_DepthGenerator;
+  xn::UserGenerator g_UserGenerator;
+  xn::Recorder* g_pRecorder;
+  
+  XnCallbackHandle hUserCBs, hCalibrationStartCB, hCalibrationCompleteCB, hPoseCBs;
+  /*g_UserGenerator.RegisterUserCallbacks(NewUser, LostUser, NULL, hUserCBs);
+  rc = g_UserGenerator.GetSkeletonCap().RegisterToCalibrationStart(CalibrationStarted, NULL, hCalibrationStartCB);
+  CHECK_RC(rc, "Register to calbiration start");
+  rc = g_UserGenerator.GetSkeletonCap().RegisterToCalibrationComplete(CalibrationCompleted, NULL, hCalibrationCompleteCB);
+  CHECK_RC(rc, "Register to calibration complete");
+  rc = g_UserGenerator.GetPoseDetectionCap().RegisterToPoseDetected(PoseDetected, NULL, hPoseCBs);
+  CHECK_RC(rc, "Register to pose detected");*/
+
+  XnStatus rc = XN_STATUS_OK;
+  xn::EnumerationErrors errors;
+
+  rc = g_Context.InitFromXmlFile(SAMPLE_XML_PATH, g_ScriptNode, &errors);
+  CHECK_ERRORS(rc, errors, "InitFromXmlFile");
+  CHECK_RC(rc, "InitFromXml");
+
+  rc = g_Context.FindExistingNode(XN_NODE_TYPE_DEPTH, g_DepthGenerator);
+  CHECK_RC(rc, "Find depth generator");
+  rc = g_Context.FindExistingNode(XN_NODE_TYPE_USER, g_UserGenerator);
+  CHECK_RC(rc, "Find user generator");
+
+  if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_SKELETON) ||
+      !g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION))
+    {
+      printf("User generator doesn't support either skeleton or pose detection.\n");
+      return XN_STATUS_ERROR;
+    }
+
+  g_UserGenerator.GetSkeletonCap().SetSkeletonProfile(XN_SKEL_PROFILE_ALL);
+  rc = g_Context.StartGeneratingAll();
+  CHECK_RC(rc, "StartGenerating");
+
+  return 0;
+}
+
 void OpenNIPlugin::Initialize()
 {
-    xn::Context g_Context;
-    xn::ScriptNode g_ScriptNode;
-    /*xn::DepthGenerator g_DepthGenerator;
-      xn::UserGenerator g_UserGenerator;
-      xn::Recorder* g_pRecorder;
+  init();
 
       XnUserID g_nPlayer = 0;
       XnBool g_bCalibrated = FALSE;
-    */
-	XnStatus rc = XN_STATUS_OK;
-	xn::EnumerationErrors errors;
+}
 
-	rc = g_Context.InitFromXmlFile(SAMPLE_XML_PATH, g_ScriptNode, &errors);
-	//CHECK_ERRORS(rc, errors, "InitFromXmlFile");
-	//CHECK_RC(rc, "InitFromXml");
+void OpenNIPlugin::Uninitialize()
+{
+}
 
-	/*rc = g_Context.FindExistingNode(XN_NODE_TYPE_DEPTH, g_DepthGenerator);
-	CHECK_RC(rc, "Find depth generator");
-	rc = g_Context.FindExistingNode(XN_NODE_TYPE_USER, g_UserGenerator);
-	CHECK_RC(rc, "Find user generator");
-
-	if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_SKELETON) ||
-		!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION))
-	{
-		printf("User generator doesn't support either skeleton or pose detection.\n");
-		return XN_STATUS_ERROR;
-	}
-
-	g_UserGenerator.GetSkeletonCap().SetSkeletonProfile(XN_SKEL_PROFILE_ALL);
-        */
-	rc = g_Context.StartGeneratingAll();
-	//CHECK_RC(rc, "StartGenerating");
+extern "C"
+{
+    DLLEXPORT void TundraPluginMain(Framework *fw)
+    {
+        Framework::SetInstance(fw); // Inside this DLL, remember the pointer to the global framework object.
+        IModule *module = new OpenNIPlugin();
+        fw->RegisterModule(module);
+    }
 }
--- src/Application/OpenNIPlugin/OpenNIPlugin.h
+++ src/Application/OpenNIPlugin/OpenNIPlugin.h
@@ -4,10 +4,6 @@
 #include "OpenNIAPI.h"
 #include "OpenNIFwd.h"
 
-#include <XnOpenNI.h>
-#include <XnCodecIDs.h>
-#include <XnCppWrapper.h>
-
 #include <QObject>
 
 class OPENNI_PLUGIN_API OpenNIPlugin : public IModule
